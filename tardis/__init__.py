57c36def20ecb6de12067bd734304309177cb5c9
ocefpaf <ocefpaf@gmail.com>
2015-09-09 15:21:15 -0300

    sos_names dictionary holding cf_names

--- tardis/__init__.py
+++ tardis/__init__.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
 
 
-from .tardis import *
+from .tardis import OceanModelCube
+from . import coords
+from . import slices
+from . import utils
 
 __version__ = '0.2.0'
--- tardis/utils.py
+++ tardis/utils.py
@@ -11,10 +11,52 @@ iris.FUTURE.netcdf_promote = True
 iris.FUTURE.cell_datetime_objects = True
 
 
-__all__ = ['wrap_lon180',
+__all__ = ['cf_name_list',
+           'wrap_lon180',
            'wrap_lon360',
            'is_model',
            'is_water']
+
+salinity = ['sea_water_salinity',
+            'sea_surface_salinity',
+            'sea_water_absolute_salinity',
+            'sea_water_practical_salinity']
+
+temperature = ['sea_water_temperature',
+               'sea_surface_temperature',
+               'sea_water_potential_temperature',
+               'equivalent_potential_temperature',
+               'sea_water_conservative_temperature',
+               'pseudo_equivalent_potential_temperature']
+
+water_level = ['sea_surface_height',
+               'sea_surface_elevation',
+               'sea_surface_height_above_geoid',
+               'sea_surface_height_above_sea_level',
+               'water_surface_height_above_reference_datum',
+               'sea_surface_height_above_reference_ellipsoid']
+
+speed_direction = ['sea_water_speed', 'direction_of_sea_water_velocity']
+
+u = ['surface_eastward_sea_water_velocity',
+     'eastward_sea_water_velocity',
+     'sea_water_x_velocity',
+     'x_sea_water_velocity',
+     'eastward_transformed_eulerian_mean_velocity',
+     'eastward_sea_water_velocity_assuming_no_tide']
+
+v = ['northward_sea_water_velocity',
+     'surface_northward_sea_water_velocity',
+     'sea_water_y_velocity',
+     'y_sea_water_velocity',
+     'northward_transformed_eulerian_mean_velocity',
+     'northward_sea_water_velocity_assuming_no_tide']
+
+cf_name_list = dict(
+    {'salinity': salinity,
+     'sea_water_temperature': temperature,
+     'currents': dict(u=u, v=v, speed_direction=speed_direction),
+     'water_surface_height_above_reference_datum': water_level})
 
 
 def wrap_lon180(lon):
